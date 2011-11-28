62430565a646389b2aa6fac78e02a76fc54e13f0
Leon Miller-Out <leon@singlebrook.com>
2011-11-28 18:34:13 -0500

    Check for player death

--- gosu/kouler.rb
+++ gosu/kouler.rb
@@ -9,6 +9,17 @@ class Player
   def initialize(window)
     @image = Gosu::Image.new(window, "media/player.png", false)
     @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @dead = false
+    @bounds = {
+      :top_left => {:x => 0, :y => 0},
+      :bottom_right => {:x => window.width, :y => window.height}
+    }
+  end
+
+  def check_for_death
+    if touching_bounds
+      @dead = true
+    end
   end
 
   def draw
@@ -16,6 +27,10 @@ class Player
   end
 
   def go(dir)
+    if @dead
+      return
+    end
+
     case dir
       when :left
         @x = @x - 3
@@ -32,6 +47,13 @@ class Player
     end
   end
 
+  def touching_bounds
+    return @x <= @bounds[:top_left][:x] || 
+           @x >= @bounds[:bottom_right][:x] ||
+           @y <= @bounds[:top_left][:y] ||
+           @y >= @bounds[:bottom_right][:y]
+  end
+
   def warp(x, y)
     @x, @y = x, y
   end
@@ -39,14 +61,18 @@ class Player
 end
 
 class GameWindow < Gosu::Window
+  attr_reader :height, :width
+
   def initialize
-    super(640, 480, false)
+    @height = 480
+    @width = 640
+    super(@width, @height, false)
     self.caption = "Kouler"
     
     @background_image = Gosu::Image.new(self, "media/space.png", true)
     
     @player = Player.new(self)
-    @player.warp(320, 240)
+    @player.warp(@width / 2, @height / 2)
   end
 
   def update
@@ -54,6 +80,7 @@ class GameWindow < Gosu::Window
     @player.go :right if button_down? Gosu::KbRight
     @player.go :down if button_down? Gosu::KbDown
     @player.go :up if button_down? Gosu::KbUp
+    @player.check_for_death
   end
 
   def draw
