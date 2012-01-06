c6fa1b3beddf24a26b0e59dfa376b931b1deb950
Leon Miller-Out <leon@singlebrook.com>
2012-01-05 19:23:00 -0500

    Added stupid chasing algorithm

--- enemy.rb
+++ enemy.rb
@@ -1,11 +1,32 @@
 class Enemy < Ball
+
+  attr_accessor :target_dx, :target_dy
+
   def initialize(window)
     super
     @image = Gosu::Image.new(window, "media/enemy.png", false)
     @radius = 10
-    @mass = 10
+    @mass = 20
     @accel = 0.3 # per tick
     @drag = 0.1 # per tick
     @velocity_max = 10
+  end
+
+  def chase(ball)
+    dx = ball.x - @x
+    dy = @y - ball.y # Backwards because of inverted Y axis
+    direction_to = Math.atan2(dx, dy) * 180 / Math::PI
+
+    thrust(direction_to)
+
+    @target_dx = dx
+    @target_dy = dy
+  end
+
+  def draw
+    super
+    # unless @dead
+    #   @window.draw_line(@x, @y, Gosu::yellow, @x + (@target_dx), @y - (@target_dy), Gosu::yellow, 100)
+    # end
   end
 end
--- player.rb
+++ player.rb
@@ -3,9 +3,9 @@ class Player < Ball
     super
     @image = Gosu::Image.new(window, "media/player.png", false)
     @radius = 24
-    @mass = 20
+    @mass = 80
     @accel = 0.3 # per tick
     @drag = 0.1 # per tick
-    @velocity_max = 4
+    @velocity_max = 5
   end
 end
